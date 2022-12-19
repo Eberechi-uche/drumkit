2a9ab833c2f68c135ac254d798bb2490e2ffcc5f
Richard Eberechi Uche <eb3rechi@gmail.com>
2022-12-19 08:49:12 +0100

    add drum image

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 
 let sounds = ['sounds/crash.mp3', 'sounds/kick-bass.mp3', 'sounds/snare.mp3', 'sounds/tom-1.mp3', 'sounds/tom-2.mp3','sounds/tom-3.mp3','sounds/tom-4.mp3']
-
+let images = ['images/crash.png','images/kick.png','images/snare.png','images/tom1.png','images/tom2.png','images/tom3.png', 'images/tom4.png' ]
+let buttons = document.querySelectorAll('.btn');
+buttons.forEach((element, index) =>{
+    let image = images[index]
+    element.style.backgroundImage = `url(${image})`
+    console.log(element.getAttribute('background-image'))
+})
 
 
 function handleClick(e){
     let audio = new Audio(sounds[e.target.id]);
-    audio.play()
-    
+    audio.play() 
 }
-let buttons = document.querySelectorAll('.btn');
-// console.log(buttons)
+
 buttons.forEach((element) => {
     element.addEventListener('click', handleClick)
     
